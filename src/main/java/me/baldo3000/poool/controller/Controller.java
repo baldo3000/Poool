6495edb700142944f431670965152390711c96eb
Andrea Baldazzi <andreabaldazzi03@gmail.com>
2026-08-01 20:26:22 +0200

    refactor: remove sketches

--- src/main/java/me/baldo3000/poool/controller/Controller.java
+++ src/main/java/me/baldo3000/poool/controller/Controller.java
@@ -31,10 +31,17 @@ public class Controller implements GameEventListener {
             eventBuffer.getLastPlayerDirection().ifPresent(board::applyInputToPlayer);
             eventBuffer.getLastCpuDirection().ifPresent(board::applyInputToCpu);
 
+            var startUpdate = System.currentTimeMillis();
             board.updateState(dt, boardUpdater);
+            var endUpdate = System.currentTimeMillis();
+            System.out.println( "Update time: " + (endUpdate - startUpdate));
 
             viewModel.update(board, dt);
+
+            var startRender = System.currentTimeMillis();
             view.render();
+            var endRender = System.currentTimeMillis();
+            // System.out.println( "Render time: " + (endRender - startRender));
         }
     }
 
